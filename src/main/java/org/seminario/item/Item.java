e61b3c3fb6c330a30da3acb3eed20a0d1c189337
macarse <macarse@755f2d98-5b70-11de-b31f-81d54249e7d9>
2009-06-20 16:23:30 +0000

    Items added.

---
+++ src/main/java/org/seminario/item/Item.java
@@ -0,0 +1,9 @@
+package org.seminario.item;
+
+
+public interface Item {
+	public void setName(String name);
+	public String getName();
+    public void setPrice(Long price);
+    public Long getPrice();
+}
--- src/main/java/org/seminario/order/Order.java
+++ src/main/java/org/seminario/order/Order.java
@@ -1,9 +1,14 @@
 package org.seminario.order;
 
+import java.util.Collection;
+
 import org.seminario.customer.Customer;
+import org.seminario.item.Item;
 
 public interface Order {
     public void setOrderNumber(long orderNumber);
     public long getOrderNumber();
+    public void addItem(Item item);
+    public Collection<Item> getItems();
     public Customer getCustomer();
 }
