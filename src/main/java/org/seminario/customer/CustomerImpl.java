4759f850d972801047e46f3b492eb63f337f617b
macarse <macarse@755f2d98-5b70-11de-b31f-81d54249e7d9>
2009-06-21 03:50:41 +0000

    benchmarks

--- src/main/java/org/seminario/customer/CustomerImpl.java
+++ src/main/java/org/seminario/customer/CustomerImpl.java
@@ -1,6 +1,12 @@
 package org.seminario.customer;
 
+import java.util.Iterator;
+
+import org.neo4j.api.core.Direction;
 import org.neo4j.api.core.Node;
+import org.neo4j.api.core.ReturnableEvaluator;
+import org.neo4j.api.core.StopEvaluator;
+import org.neo4j.api.core.Traverser;
 import org.seminario.order.Order;
 import org.seminario.order.OrderImpl;
 import org.seminario.relationships.RelationshipTypes;
@@ -33,5 +39,33 @@ public class CustomerImpl implements Customer {
 		Node orderNode = ((OrderImpl) order).getUnderlyingNode();
 		getUnderlyingNode().createRelationshipTo(orderNode,
 				RelationshipTypes.CUSTOMER_TO_ORDER);
+	}
+
+	@Override
+	public Iterator<Order> getAllOrders() {
+		return new Iterator<Order>() {
+			private final Iterator<Node> iterator = getUnderlyingNode()
+					.traverse(Traverser.Order.BREADTH_FIRST,
+							StopEvaluator.DEPTH_ONE,
+							ReturnableEvaluator.ALL_BUT_START_NODE,
+							RelationshipTypes.CUSTOMER_TO_ORDER,
+							Direction.OUTGOING).iterator();
+
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
+
+			@Override
+			public Order next() {
+				Node nextNode = iterator.next();
+				return new OrderImpl(nextNode);
+			}
+
+			@Override
+			public void remove() {
+				iterator.remove();
+			}
+		};
 	}
 }
