048622ed990d1f61ae22398934403ef23e2a6546
macarse <macarse@755f2d98-5b70-11de-b31f-81d54249e7d9>
2009-06-25 15:37:03 +0000

    Ejemplo de roberto carlos con mysql

---
+++ trunk/src/main/java/org/robertoCarlos/RobertoCarlosJava.java
@@ -0,0 +1,81 @@
+package org.robertoCarlos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+public class RobertoCarlosJava {
+
+	public static long start;
+	public static long end;
+
+	public static void main(String[] args) {
+		RobertoCarlosJava mydb = new RobertoCarlosJava();
+		mydb.connect();
+	}
+
+	private void connect() {
+		Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        Integer cant = 0;
+        Random r = new Random();
+        String[] names = new String[] { "Diego", "Carlos", "Pipo", "Maxito",
+				"Juani", "Pablo", "Martín" };
+        
+        start = System.currentTimeMillis();
+        try {
+              Class.forName("com.mysql.jdbc.Driver");
+              String url = "jdbc:mysql://localhost:3306/neo4j";
+
+              conn = DriverManager.getConnection(url, "neo4j", "neo4j");
+              stmt = conn.createStatement();
+              stmt.execute("DROP TABLE amigos");
+              stmt.execute(	"CREATE TABLE amigos" +
+              				"("+
+            		  		"id          int AUTO_INCREMENT          not null,"+
+            		  		"nombre      char(20)                    not null,"+
+            		  		"primary key(id)" +
+            		  		")");
+
+        	for (int i = 0; i < 100000; i++) {
+  				
+  				int nombreIndex = r.nextInt(names.length);
+  				String name = names[nombreIndex] + i;
+
+  				stmt.execute("INSERT INTO amigos (nombre) VALUES ('" +name+ "')");
+			}
+            end = System.currentTimeMillis();
+            System.out.println("La db se creó en " + (end - start) + " ms.");
+
+    		start = System.currentTimeMillis();
+    		rs = stmt.executeQuery("select count(*) as count from amigos");
+    		while(rs.next()) {
+    			cant = rs.getInt("count");
+    		}
+
+        } catch (ClassNotFoundException exception) {
+              exception.printStackTrace();
+        } catch(SQLException sqlex) {
+              sqlex.printStackTrace();
+        } finally {
+              try {
+                    rs.close();
+              } catch(Exception e) {}
+              try {
+                    stmt.close();
+              } catch(Exception e) {}
+              try {
+                    conn.close();
+              } catch(Exception e) {}
+        }
+
+		System.out.println("Cantidad de amigos " + cant);
+		end = System.currentTimeMillis();
+
+		System.out.println("La consulta se hizo en " + (end - start) + " ms.");
+	}
+}
